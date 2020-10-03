2b5ea0b39f0b717fa1ba1beb1559690a10da8f72
HalynaHera <gerasymenko_ba12@nuwm.edu.ua>
2020-10-03 22:45:46 +0300

    feat: implement Typical Arrays Problems scripts

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array) return 0; 
+  if(array.length === 0) return 0;
+  let minimum = null;
+  array.forEach(element => {
+    if(minimum > element || !minimum) minimum = element;
+  });
+  return minimum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array) return 0; 
+  if(array.length === 0) return 0;
+  let maxi = null;
+  array.forEach(element => {
+    if(maxi < element || !maxi) maxi = element;
+  });
+  return maxi;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array) return 0; 
+  if(array.length === 0) return 0;
+  return array.reduce((a, b) => a + b) / array.length;
 }
